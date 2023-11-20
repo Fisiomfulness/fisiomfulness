283fbc003753840487166e19c3fc6026ddcd4cfa
Sebatora <sebastiana.toranzo@gmail.com>
2023-11-20 15:56:07 -0300

    nextUI config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
+const {nextui} = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    colors: {
+      primary: "#112732",
+      secondary: "#3397C6",
+      "primary-opacity": "rgb(17, 39, 50, 0.5)",
+      "light-blue": "#ACD9EC",
+      "action-green": "#36A793",
+      "action-blue": "#62CFE4",
+      "primary-hover": "rgb(17, 39, 50, 0.7)",
+      "action-hover": "rgb(54, 167, 147, 0.7)",
+      "pale-blue": "#F2F8FA",
+      "pale-blue-opacity": "rgb(242, 248, 250, 0.5)",
+      white: "#FAFAFA",
+      black: "#0D0D0D",
     },
   },
-  plugins: [],
-}
+};
+export const darkMode = "class";
+export const plugins = [nextui()];
