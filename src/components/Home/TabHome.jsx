5ab0dce0754e75f8a495c82c6b5fe2b5cfeb9a2c
Luciano E. Perez <info.luciando.design@gmail.com>
2023-11-23 15:16:46 -0300

    tab-home estilizado

--- src/components/Home/TabHome.jsx
+++ src/components/Home/TabHome.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { AiFillHome } from "react-icons/ai";
 import { BiSolidWebcam } from "react-icons/bi";
+import { CiLocationOn } from "react-icons/ci";
+import { FaUserDoctor } from "react-icons/fa6";
+
 import {
   Tabs,
   Tab,
@@ -18,8 +21,8 @@ export default function TabHome() {
 
   return (
     <div className=" flex items-center ">
-      <Card  className="max-w-full  md:w-[900px] md:h-[150px] sm:w-[500px] sm:h-[260px] h-[300px]">
-        <CardBody className="overflow-hidden" >
+      <Card className="max-w-full  md:w-[900px] md:h-[150px] sm:w-[500px] sm:h-[260px] h-[300px]">
+        <CardBody className="overflow-hidden">
           <Tabs
             fullWidth
             color="primary"
@@ -27,27 +30,35 @@ export default function TabHome() {
             aria-label="Tabs form"
             selectedKey={selected}
             onSelectionChange={setSelected}
-            
           >
-            <Tab  key="domiciliaria" title={
-              <div className="flex relative items-center space-x-2">
-                <AiFillHome className=""/>
-                <span>Cita Domiciliaria</span>
-              </div>
-            } className="h-full" >
+            <Tab
+              key="domiciliaria"
+              title={
+                <div className="flex relative items-center space-x-2">
+                  <AiFillHome className="" />
+                  <span>Cita Domiciliaria</span>
+                </div>
+              }
+              className="h-full"
+            >
               <form className="flex md:flex-row h-full flex-col justify-center items-center gap-4">
-              
                 <div className="flex md:flex-row flex-col flex-1">
                   <Autocomplete
-                  startsWidth={<AiFillHome />}
+                    startsWidth={<AiFillHome />}
                     label="Seleccione:"
                     placeholder="Especialidad"
                     className="max-w-xs md:mr-2 mr-0"
                     defaultItems={especialidadesMedicas}
+                    listboxProps={{
+                      color: "primary",
+                    }}
                   >
                     {(item) => (
                       <AutocompleteItem key={item.value}>
-                        {item.label}
+                        <div className="flex items-center gap-2">
+                          <FaUserDoctor alt={item.label} className="text-action-blue"/>
+                          <span>{item.label}</span>
+                        </div>
                       </AutocompleteItem>
                     )}
                   </Autocomplete>
@@ -56,27 +67,37 @@ export default function TabHome() {
                     placeholder="Ciudad"
                     className="max-w-xs md:mt-0 mt-2"
                     defaultItems={ciudadesPeru}
+                    listboxProps={{
+                      color: "primary",
+                    }}
                   >
                     {(item) => (
                       <AutocompleteItem key={item.value}>
-                        {item.label}
+                        <div className="flex items-center gap-2">
+                          <CiLocationOn alt={item.label} className="text-action-blue"  />
+                          <span>{item.label}</span>
+                        </div>
                       </AutocompleteItem>
                     )}
                   </Autocomplete>
                 </div>
                 <div className="flex basis-1/5">
-                  <Button fullWidth color="primary" >
+                  <Button fullWidth color="primary">
                     Buscar
                   </Button>
                 </div>
               </form>
             </Tab>
-            <Tab key="online" title={
-              <div className="flex relative items-center space-x-2">
-              <BiSolidWebcam className=""/>
-              <span>Cita Online</span>
-            </div>
-            } className="h-full">
+            <Tab
+              key="online"
+              title={
+                <div className="flex relative items-center space-x-2">
+                  <BiSolidWebcam className="" />
+                  <span>Cita Online</span>
+                </div>
+              }
+              className="h-full"
+            >
               <form className="flex md:flex-row flex-col h-full justify-between  items-center gap-4">
                 <div className="flex mt-10 md:mt-0">
                   <Autocomplete
@@ -84,8 +105,6 @@ export default function TabHome() {
                     defaultItems={especialidadesMedicas}
                     label="Seleccione:"
                     placeholder="Especialidad"
-                    
-                    
                   >
                     {(especialidadesMedicas) => (
                       <SelectItem key={especialidadesMedicas.value}>
