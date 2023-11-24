3b25f3929dde8d71fb1cf391d69c678927227902
EnzoMarinich <marinichenzo@gmail.com>
2023-11-24 17:11:11 -0300

    refactor: :art: page product remodeling

--- src/app/productos/page.jsx
+++ src/app/productos/page.jsx
@@ -1,8 +1,7 @@
 import data from "@/components/productos/data/productos.json";
-
 import ProductClient from "@/components/productos/ProductClient";
 
-const ProductosPage = () => {
+const ProductosPage = async () => {
   return (
     <>
       <ProductClient data={data.productos} />
