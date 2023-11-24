afea6df300db050bec89ae12c95c7836c02eed72
Sebatora <sebastiana.toranzo@gmail.com>
2023-11-24 15:15:47 -0300

    error 404

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,10 +8,12 @@ export default function NotFound() {
         <h2 className="text-8xl text-primary">4</h2>
         <h2 className="text-8xl text-[#3DAADD]">0</h2>
         <h2 className="text-8xl text-primary">4</h2>
+
         <div className="flex items-baseline w-16 h-16 absolute -right-14">
           <Image width={50} height={50} src={Error404} alt="Error 404" />
         </div>
       </div>
+
       <div className="flex flex-col m-4 text-sm items-center justify-center">
         <p className="m-1">
           Es posible que la entrada haya sido eliminada o que la dirección no
