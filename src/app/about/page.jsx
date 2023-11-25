35fc6e1cfc30e6f09a23c8dfbc38ba860da211dd
Mauri <mauri.monzon91@gmail.com>
2023-11-25 20:45:08 -0300

    Assets

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div className="w-full h-[100vh] bg-[#3DAADD]">
+        <div className="w-full h-[100vh] bg-red-400 rounded-tl-full rounded-tr-none"></div>
+      </div>
+    );
+  }
+  
