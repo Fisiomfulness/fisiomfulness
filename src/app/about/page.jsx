b6e6bc5ba0a4fec0a627848553a613a35bbdaa1f
Mauri <mauri.monzon91@gmail.com>
2023-11-25 21:00:13 -0300

    Componente ImgAbout

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,15 @@
+import imgTop from "@/assets/about/top.svg";
+import imgBot from "@/assets/about/bot.svg";
+import ImgAbout from "./imgAbout";
+
 export default function About() {
-    return (
-      <div className="w-full h-[100vh] bg-[#3DAADD]">
-        <div className="w-full h-[100vh] bg-red-400 rounded-tl-full rounded-tr-none"></div>
+  return (
+    <div className="w-full min-h-screen relative">
+      <ImgAbout image={imgTop} />
+      <div className="w-full h-full flex items-center">
+        <h1>Quienes somos</h1>
       </div>
-    );
-  }
-  
+      <ImgAbout image={imgBot} />
+    </div>
+  );
+}
