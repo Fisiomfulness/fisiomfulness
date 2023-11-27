dc8b96b4bfe1f019edd23b46f1fa18aebe9dc54a
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-11-27 10:40:54 -0300

    blog

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardBlog from '@/components/CardBlog';
+import BlogList from '@/components/BlogList';
+
+const Page = () => {
+  return (
+    <div className="flex flex-col lg:flex-row justify-start items-start md:ml-20 ">
+      <CardBlog className="lg:w-1/2" />
+      <BlogList className="lg:w-full " /> 
+    </div>
+  );
+};
+
+export default Page;
