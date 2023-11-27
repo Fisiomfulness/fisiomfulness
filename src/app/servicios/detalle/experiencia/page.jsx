0c932148bf2e42225d5b9f9831c40cca2a59bfda
Luciano E. Perez <info.luciando.design@gmail.com>
2023-11-27 14:36:01 -0300

    servicios-aside

---
+++ src/app/servicios/detalle/experiencia/page.jsx
@@ -0,0 +1,17 @@
+import ServicioAsideBar from "@/components/Servicios/ServicioAsideBar"
+
+const ServicioExperiencia = () => {
+  return (
+    <div className="flex flex-col md:flex-row justify-around">
+      <div>
+        <ServicioAsideBar/>
+      </div>
+      <div>
+
+        <h1> Experiencia</h1>
+      </div>
+    </div>
+  )
+}
+
+export default ServicioExperiencia
--- src/components/Servicios/ServicioAsideBar.jsx
+++ src/components/Servicios/ServicioAsideBar.jsx
@@ -1,28 +1,35 @@
-'use client'
+"use client";
 import { usePathname } from "next/navigation";
 import { Button } from "@nextui-org/react";
+import NextLink from "next/link";
 
 const ServicioAsideBar = () => {
-    const path = usePathname()
-    console.log(path)
+  const path = usePathname();
+  console.log(path);
+
+  const buttons = [
+    { name: "Perfil profesional", href: "/servicios/detalle/perfil",color:"secondary" ,className: "text-white rounded-l-md md:rounded-t-md md:rounded-l-none hover:text-white" },
+    { name: "precios", href: "/servicios/detalle/precios", color: "secondary", className:"hover:text-white" },
+    { name: "experiencia", href: "/servicios/detalle/experiencia", color: "secondary",  className:"hover:text-white"},
+    { name: "turno", href: "/servicios/detalle/turno", className: "rounded-r-md   md:rounded-r-none md:rounded-b-md hover:text-white", color: "secondary" },
+  ];
+
   return (
     <aside className="flex justify-center ">
       <div className="flex flex-row md:flex-col">
-        <Button
-          radius="none"
-          className="bg-action-green text-white rounded-l-md md:rounded-t-md md:rounded-l-none"
-        >
-          Perfil Profesional
-        </Button>
-        <Button color="secondary" radius="none">
-          Precios
-        </Button>
-        <Button color="secondary" radius="none">
-          Experiencia
-        </Button>
-        <Button className="rounded-r-md md:rounded-r-none md:rounded-b-md" color="secondary" radius="none">
-          Turno
-        </Button>
+        {buttons.map((button, index) => (
+          <Button
+            key={index}
+            name={button.name}
+            as={NextLink}
+            href={button.href}
+            color={path === button.href ? "primary" : button.color}
+            radius="none"
+            className={button.className}
+          >
+            {button.name}
+          </Button>
+        ))}
       </div>
     </aside>
   );
