e71fd567805b939516c3f82b1e7c0a63bbf05d37
Lauty892 <lauta_lauta@hotmail.com>
2023-11-29 20:54:27 -0300

    tratments

--- src/app/tratamientos/Treatments.jsx
+++ src/app/tratamientos/Treatments.jsx
@@ -4,13 +4,13 @@ import data from "./assets/treatmentsList.json";
 
 const Treatments = () => {
   return (
-<content className="flex flex-wrap gap-4 h-[34rem] w-[850px]">
+<content className="flex flex-wrap gap-4 p-4 h-[34rem] mx-auto md:w-[764px] w-[350px] md:mb-[0px] mb-[600px]">
   {data.results.map((el, index) => (
     <Card
       key={index}
       isFooterBlurred
       radius="sm"
-      className="border-none flex-wrap min-w-[200px] flex-1 transition-transform transform hover:scale-105"
+      className="border-none flex-wrap min-w-[200px] min-h-[200px] flex-1 transition-transform transform hover:scale-105"
       style={{ backgroundImage: `url(${el.img})`, backgroundSize: 'cover', }}
     >
 
---
+++ src/app/tratamientos/page.jsx
@@ -0,0 +1,6 @@
+import Tratments from './Treatments';
+const tratamientos= () => {
+  return <Tratments />
+}
+
+export default tratamientos;
