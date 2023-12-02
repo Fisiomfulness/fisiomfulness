715fa84b977d6cf3ce794e0d96e291e6c1e91218
tm10ymhp <tm10ymhp@protonmail.com>
2023-12-01 21:29:16 -0500

    23.1201.2129

--- src/app/mis_productos/page.jsx
+++ src/app/mis_productos/page.jsx
@@ -6,8 +6,8 @@ import { Avatar } from "@nextui-org/react";
 
 export default function MisProductosPage() {
 	return (
-		<div className="flex flex-row p-4 bg-blue-100 max-h-[520px] max-w-fit mx-auto">
-			<div className="flex flex-col overflow-y-auto min-w-md gap-2 bg-red-300">
+		<div className="flex flex-row p-4 max-h-[520px] max-w-fit mx-auto">
+			<div className="flex flex-col overflow-y-auto min-w-md gap-2">
 				{Array(10)
 					.fill(0)
 					.map((_, i) => (
@@ -16,6 +16,7 @@ export default function MisProductosPage() {
 							className={cn(
 								"bg-primary-light px-6 py-2 rounded-lg text-black w-64 mr-4",
 								"flex items-center",
+								2 === i && "bg-secondary-light text-white",
 							)}
 						>
 							<Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
