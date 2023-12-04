561f76387761d3d15fb1615313e6fe206147630a
tm10ymhp <tm10ymhp@protonmail.com>
2023-12-04 16:52:07 -0500

    fix: Corrige indentado

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -4,9 +4,9 @@ import { NextUIProvider } from "@nextui-org/react";
 import { Provider as JotaiProvider } from "jotai";
 
 export function Providers({ children }) {
-	return (
-		<NextUIProvider>
-			<JotaiProvider>{children}</JotaiProvider>
-		</NextUIProvider>
-	);
+  return (
+    <NextUIProvider>
+      <JotaiProvider>{children}</JotaiProvider>
+    </NextUIProvider>
+  );
 }
