a5703ac8433ccbfad427b3389446e34e008956dc
tm10ymhp <tm10ymhp@protonmail.com>
2023-12-05 16:29:48 -0500

    feat: Agrega vista `mis_productos`

--- src/app/mis_mensajes/page.jsx
+++ src/app/mis_mensajes/page.jsx
@@ -5,26 +5,26 @@ import { cn } from "@/features/ui";
 import { Avatar } from "@nextui-org/react";
 
 export default function MisProductosPage() {
-	return (
-		<div className="flex flex-row p-4 max-h-[520px] max-w-fit mx-auto">
-			<div className="flex flex-col overflow-y-auto min-w-md gap-2">
-				{Array(10)
-					.fill(0)
-					.map((_, i) => (
-						<div
-							key={i}
-							className={cn(
-								"bg-primary-light px-6 py-2 rounded-lg text-black w-64 mr-4",
-								"flex items-center",
-								2 === i && "bg-secondary-light text-white",
-							)}
-						>
-							<Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-							<p className="text-lg mx-auto">Ronald Marino</p>
-						</div>
-					))}
-			</div>
-			<Chat />
-		</div>
-	);
+  return (
+    <div className="flex flex-row p-4 max-h-[520px] max-w-fit mx-auto">
+      <div className="flex flex-col overflow-y-auto min-w-md gap-2">
+        {Array(10)
+          .fill(0)
+          .map((_, i) => (
+            <div
+              key={i}
+              className={cn(
+                "bg-primary-light px-6 py-2 rounded-lg text-black w-64 mr-4",
+                "flex items-center",
+                2 === i && "bg-secondary-light text-white",
+              )}
+            >
+              <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+              <p className="text-lg mx-auto">Ronald Marino</p>
+            </div>
+          ))}
+      </div>
+      <Chat />
+    </div>
+  );
 }
---
+++ src/features/ui/components/CustomTable/CustomTableHeader.jsx
@@ -0,0 +1,21 @@
+import { cn } from "../../utils";
+
+export default function CustomTableHeader({ headers }) {
+  return (
+    <thead className="text-white bg-primary text-center">
+      <tr>
+        {headers.map((header) => (
+          <th
+            key={header}
+            className={cn(
+              "px-6 py-3 border-white even:bg-[#2984AE]",
+              "border-r-2 last:border-r-0 font-normal",
+            )}
+          >
+            {header}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+}
