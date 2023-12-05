ec78ba2dfdc9027269fa96151405861f19db9f63
Alejandro Maturrano <tm10ymhp@protonmail.com>
2023-12-05 15:15:43 -0500

    feat: Agregar `@swc-jotai/react-refresh`, estilos de tailwind y ajustes de formato (#43)

--- src/app/carrito/page.jsx
+++ src/app/carrito/page.jsx
@@ -1,9 +1,9 @@
 import { Pago } from "@/features/carrito";
 
 export default function CarritoPage() {
-	return (
-		<div className="flex flex-col justify-center items-center min-h-[80vh] px-4 gap-8">
-			<Pago />
-		</div>
-	);
+  return (
+    <div className="flex flex-col justify-center items-center min-h-[80vh] px-4 gap-8">
+      <Pago />
+    </div>
+  );
 }
--- src/features/carrito/hooks/index.js
+++ src/features/carrito/hooks/index.js
@@ -3,22 +3,22 @@ import { addItemAtom, cartAtom, clearCartAtom, removeItemAtom } from "../store";
 import { useMemo } from "react";
 
 export function useCart() {
-	const _state = useAtomValue(cartAtom);
+  const _state = useAtomValue(cartAtom);
 
-	const removeItem = useSetAtom(removeItemAtom);
-	const addItem = useSetAtom(addItemAtom);
-	const clearCart = useSetAtom(clearCartAtom);
+  const removeItem = useSetAtom(removeItemAtom);
+  const addItem = useSetAtom(addItemAtom);
+  const clearCart = useSetAtom(clearCartAtom);
 
-	const state = useMemo(() => _state, [_state]);
+  const state = useMemo(() => _state, [_state]);
 
-	const actions = useMemo(
-		() => ({
-			removeItem,
-			addItem,
-			clearCart,
-		}),
-		[removeItem, addItem, clearCart],
-	);
+  const actions = useMemo(
+    () => ({
+      removeItem,
+      addItem,
+      clearCart,
+    }),
+    [removeItem, addItem, clearCart],
+  );
 
-	return [state, actions];
+  return [state, actions];
 }
