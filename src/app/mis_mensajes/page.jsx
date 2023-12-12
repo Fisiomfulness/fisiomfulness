f66a12441ef0ce8497d7239c6ca33d12e42534e5
EnzoMarinich <marinichenzo@gmail.com>
2023-12-12 16:57:17 -0300

    feat: :sparkles: add mesagge in comunidad

--- src/app/mis_mensajes/page.jsx
+++ src/app/mis_mensajes/page.jsx
@@ -6,7 +6,7 @@ import { Avatar } from "@nextui-org/react";
 
 export default function MisProductosPage() {
   return (
-    <div className="flex flex-row p-4 max-h-[520px] max-w-fit mx-auto">
+    <div className="flex flex-row p-4 max-h-[520px] max-w-fit mx-auto mt-10 mb-28">
       <div className="flex flex-col overflow-y-auto min-w-md gap-2">
         {Array(10)
           .fill(0)
@@ -16,7 +16,7 @@ export default function MisProductosPage() {
               className={cn(
                 "bg-primary-light px-6 py-2 rounded-lg text-black w-64 mr-4",
                 "flex items-center",
-                2 === i && "bg-secondary-light text-white",
+                2 === i && "bg-secondary-light text-white"
               )}
             >
               <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
