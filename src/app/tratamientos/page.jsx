d6a897d5865d155cd577153ef54f39485b18a97f
tm10ymhp <tm10ymhp@protonmail.com>
2024-01-29 11:57:21 -0500

    Revert "Merge pull request #76 from Fisiomfulness/Treeatments"
    This reverts commit d5781882ca4fd41940474dd0164f385334f6aa8b, reversing
    changes made to 9ae7185b6bc9ec8a5cc425d805e465ea9a528791.

--- src/app/tratamientos/page.jsx
+++ src/app/tratamientos/page.jsx
@@ -1,12 +1,12 @@
-
-import TreatmentClient from "@/components/Tratamientos/TreatmentClient";
-
+import TreatmentCardContainer from "@/components/Tratamientos/TreatmentCardContainer";
 import data from "@/components/Tratamientos/data/treatmentsList.json";
 
 const TreatmentsPage = () => {
   return (
     <div className="w-full min-h-screen mb-10 flex flex-row gap-5 pt-6 md:pr-5 md:pl-5 justify-center">
-      <TreatmentClient data={data.treatments} />
+      <div className="flex flex-col min-h-screen items-center gap-10 mt-5 pb-10 justify-start">
+        <TreatmentCardContainer treats={data.treatments} />
+      </div>
     </div>
   );
 };
